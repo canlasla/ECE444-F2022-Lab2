e0050cc0388435158f4bf6f34550452363ecf354
canlasla <lancecanlas25@gmail.com>
2022-09-25 00:59:22 -0400

    Created nav bar, reports local current time and time since page rendered

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ This repo is a clone of https://github.com/miguelgrinberg/flasky
 ** Activity 2**
 
 ![](images/Activity2.jpg)
+
+** Activity 3**
+![](images/Activity3.jpg)
--- hello.py
+++ hello.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime as dt
 
 app = Flask(__name__)
+bootstrap=Bootstrap(app)
+moment=Moment(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    # now=dt.now()
+    # rendered_time = now.strftime("%A, %B %d, %Y %H:%M %p")
+    # print(rendered_time)
+    return render_template('index.html', current_time=dt.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
+
